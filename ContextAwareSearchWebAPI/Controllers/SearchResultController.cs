00b042e94aafd195fbc3e212f912d65ad59a070d
ericve25 <ericve25@gmail.com>
2014-09-20 09:52:07 -0700

    Initial service commit

---
+++ ContextAwareSearchWebAPI/Controllers/SearchResultController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Services.Client;
+using System.Configuration;
+using ContextAwareSearchWebAPI.Models;
+
+namespace ContextAwareSearchWebAPI.Controllers
+{
+    public class SearchResultController : ApiController
+    {
+        [HttpGet]
+        public SearchResult[] ContextAwareSearch([FromUri] string query, [FromUri] string[] keywords)
+        {
+            query = query.Trim();
+            List<string> queryModifications = new List<string>();
+            queryModifications.Add("learning " + query);
+            queryModifications.Add(query + " basics");
+            queryModifications.Add(query + " research");
+            queryModifications.Add(query + " courses");
+            queryModifications.Add(query + " events");
+            queryModifications.Add("how to learn " + query);
+
+            string[] context = {"coursera", "instructables"};
+
+            //Set default web proxy - ONLY NEEDED FOR 1AND1 HOSTING
+            //WebRequest.DefaultWebProxy = new WebProxy("ntproxyus.lxa.perfora.net", 3128);
+
+            //Set up Bing connection
+            string rootUri = "https://api.datamarket.azure.com/Bing/Search";
+            var bingContainer = new Bing.BingSearchContainer(new Uri(rootUri));
+            var accountKey = ConfigurationManager.AppSettings["BING_KEY"];
+            bingContainer.Credentials = new NetworkCredential(accountKey, accountKey);
+
+            //Set up search results list
+            List<IEnumerable<Bing.WebResult>> searchResults = new List<IEnumerable<Bing.WebResult>>();
+
+            //Search for given topic
+            DataServiceQuery<Bing.WebResult> webQuery = bingContainer.Web(query, null, null, "en-us", null, null, null, null);
+            webQuery = webQuery.AddQueryOption("$top", 10);
+            searchResults.Add(webQuery.Execute());
+
+            //Search for keywords
+            foreach (string keyword in keywords)
+            {
+                webQuery = bingContainer.Web(query + keyword.Trim(), null, null, "en-us", null, null, null, null);
+                webQuery = webQuery.AddQueryOption("$top", 10);
+                searchResults.Add(webQuery.Execute());
+            }
+
+            //Add using query modifications
+            foreach (string queryMod in queryModifications)
+            {
+                webQuery = bingContainer.Web(queryMod, null, null, "en-us", null, null, null, null);
+                webQuery = webQuery.AddQueryOption("$top", 10);
+                searchResults.Add(webQuery.Execute());
+            }
+
+            //Parse search results
+            List<SearchResult> items = new List<SearchResult>();
+            int listNumber = 1;
+            for (int i = 0; i < searchResults.Count; i++)
+            {
+                int initialRank = listNumber;
+                foreach (Bing.WebResult result in searchResults[i])
+                {
+                    int rank = initialRank;
+                    SearchResult temp = new SearchResult();
+                    temp.title = result.Title;
+                    temp.description = result.Description;
+                    temp.url = result.Url;
+                    
+                    //Modify rank based on user preferences
+                    foreach (string keyword in keywords)
+                    {
+                        if (result.Title.Contains(keyword))
+                        {
+                            rank = rank / 4;
+                        }
+                        else if (result.Description.Contains(keyword))
+                        {
+                            rank = rank / 2;
+                        }
+                    }
+
+                    //Modify rank based on static context
+                    foreach (string word in context)
+                    {
+                        if (result.Url.Contains(word))
+                        {
+                            rank = rank / 10;
+                        }
+                        else if (result.Title.Contains(word))
+                        {
+                            rank = rank / 4;
+                        }
+                        else if (result.Description.Contains(word))
+                        {
+                            rank = rank / 2;
+                        }
+                    }
+
+                    temp.ranking = rank;
+                    items.Add(temp);
+                    initialRank += 100;
+                }
+                listNumber++;
+
+                
+            }
+
+            //Sort results by rank
+            items.Sort((s1, s2) => s1.ranking.CompareTo(s2.ranking));
+            List<string> results = new List<string>();
+            foreach (SearchResult item in items)
+            {
+                results.Add(item.title + "\n" + item.description + "\n" + item.url + "\n\n");
+            }
+
+            SearchResult[] resultArray = items.ToArray();
+            return resultArray;
+        }
+    }
+}
---
+++ ContextAwareSearchWebAPI/Models/SearchResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContextAwareSearchWebAPI.Models
+{
+    public class SearchResult
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string url { get; set; }
+        public int ranking { get; set; }
+    }
+}
